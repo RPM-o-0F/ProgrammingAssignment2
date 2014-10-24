e204333e636451b19e092923625d092fcf1ce366
RPM-o-0F <email.rpm@gmail.com>
2014-10-23 23:34:48 -0500

    First try to commit to my repository

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+#These functions illustrate how the use of the super-assignment operator (<<-)
+#can be used to utilize scoping rules in R functions.
 
-## Write a short comment describing this function
+# This function establishes the matrix and a list of functions that includes
+# getting and setting the matrix and getting and setting the inverse of 
+#the matrix.  
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL #initializes m
+        set <- function(y) {
+                x <<- y #assigns x in the calling environment
+                m <<- NULL #assigns m in the calling environment
+        }
+        get <- function() x # auto print of x
+        setinverse <- function(solve) m <<- solve # assigns m in calling env.
+        getinverse <- function() m # auto print of m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+#This function will take advantage of a previously solved inverse matrix 
+#to save from solving for the inverse matrix unnecessarily 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        m <- x$getinverse() #calls for existing value of inverse
+        if(!is.null(m)) { #if a value exist, use it
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get() #if value doesnt exist then solve for inverse matrix
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m}
